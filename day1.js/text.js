078b6bda2e4f380d87b900a27705146bc8a90114
Rohit Sharma <82994526+rohitvlog@users.noreply.github.com>
2023-10-16 19:17:34 +0000

    fibonacciseries.js

--- day1.js/text.js
+++ day1.js/text.js
@@ -1,1 +1,14 @@
-console.log('rohit sharma ');
+const prompt = require('prompt-sync')();
+
+const number = prompt('Enter the number :');
+let n1=0 , n2 = 1, nextTerm;
+
+console.log("Fibonacci Series");
+
+for(let i=0; i<=number;i++)
+{
+    console.log(n1);
+    nextTerm = n1+n2;
+    n1=n2;
+    n2=nextTerm;
+}
