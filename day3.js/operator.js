4afb0e8912ce8581ed5005d305b5af9b102e297f
Rohit Sharma <82994526+rohitvlog@users.noreply.github.com>
2023-10-20 06:48:38 +0000

    operator

---
+++ day3.js/operator.js
@@ -0,0 +1,198 @@
+//arithmetic operator//
+
+// addition operator//
+let q=9;
+let k=10;
+console.log(q+k);
+
+//subtraction operator//
+let a =10;
+let b =15;
+console.log(a-b);
+
+//multiplication operator//
+let h=3;
+let g=4;
+console.log(h*g);
+
+//division operator//
+let r=9;
+let s=3;
+console.log(r/s);
+
+
+//modulas operator//
+let m=10;
+let n=20;
+console.log(m%n);
+
+//increment operator//
+let x=4;
+z=x++;
+console.log(x);
+
+//decrement operator//
+let c=5;
+d=c++;
+console.log(c);
+
+
+
+// assignment operator//
+
+//assign operator//
+let a1=10;
+let a2=20;
+console.log(a1=a2);
+
+//addition assign oerator//
+let b1=13;
+let b2=14;
+console.log(b1+=b2);
+
+
+//subtraction assign operator//
+let c1=44;
+let c2=4;
+console.log(c1-=c2);
+
+//multiplication assign operator//
+let d1=3;
+let d2=4;
+console.log(d1*=d2);
+
+//division assign operator//
+let e1=9;
+let e2=2;
+console.log(e1/=e2);
+
+//modulas operator//
+let f1=22;
+let f2=20;
+console.log(f1%=f2);
+
+
+
+
+//logical operator//
+
+//and operator//
+let g1=2;
+let g2=4;
+console.log(g1&g2);
+
+//or operator//
+let h1=4;
+let h2=9;
+console.log(h1|h2);
+
+//not operator//
+let i1=29;
+let i2=8;
+console.log(!i2);
+
+
+
+
+//comparison operator//
+
+//is equal to operator//
+let j1=17;
+let j2=17;
+console.log(j1==j2);
+
+//identical operator//
+let k1=22;
+let k2=23;
+console.log(k1===k2);
+
+//isnotequalto//
+let l1=34;
+let l2=42;
+console.log(l1!=l2);
+
+//greater than operator//
+let m1=61;
+let m2=43;
+console.log(m1<m2);
+
+//less than operator//
+let n1=43;
+let n2=23;
+console.log(n1>n2);
+
+//greater than or equal to//
+let o1=25;
+let o2=34;
+console.log(o1<=o2);
+
+//less than or equal to//
+let p1=45;
+let p2=92;
+console.log(p1<=p2);
+
+
+
+
+//bitwise operator//
+
+//and operator//
+let q1=55;
+let q2=34;
+console.log(q1&q2);
+
+//or operator//
+let r1=23;
+let r2=45;
+console.log(r1|r2);
+
+//not operator//
+let s1=49;
+let s2=90;
+console.log(~s2);
+
+//xor operator//
+let t1=78;
+let t2=46;
+console.log(t1^t2);
+
+//left shift//
+let u1=34;
+let u2=89;
+console.log(u1<<u2);
+
+//right shift//
+let v1=56;
+let v2=45;
+console.log(u1>>u2);   // 
+
+// string operator//
+let w1="rohit";
+let w2="sharma";
+let result =(w1 + w2);
+console.log(result); 
+
+
+
+//concetenate assignment operator//
+let y1='rohit';
+let y2='sharma';
+
+y1 += y2;
+console.log(y1);
+
+
+
+
+
+//ternary operator//
+function abc(){
+    let x1marks  = 42;
+    let result =(x1marks>39)?
+    "pass" : "fail";
+    console.log(result);
+
+ }
+    abc();
+      
+
