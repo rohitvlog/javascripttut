cebac0421eff01401c30c7a948813ab4a51cd315
Rohit Sharma <82994526+rohitvlog@users.noreply.github.com>
2023-10-20 07:27:47 +0000

    loops

---
+++ day6.js/loops.js
@@ -0,0 +1,60 @@
+
+
+for(let i=2;i<=4;i++)
+{
+    console.log("value of i:"+i);
+}
+
+
+
+//for loop if you want to run the same code again and again but each times values will be different
+// while loop - while loops firstly check the condition first and they will execute.
+// do while loop- do while loop execute the statement at least once and then checked the condition.
+// for in for in is basically used for properties of an object.
+//for of - for of is basically used for iterable values of an object.
+
+
+// while loop//
+
+let val=1;
+
+while(val <=  6)
+{   
+    console.log(val);   
+  val++;
+}
+
+
+// for loop code in javascript//
+
+    const n=5;
+
+    for(let i=0;i<=10;i++)
+    {
+        console.log(`i*5`);
+    }
+
+
+//for loop code in javascript -2)
+
+const r=9;
+for(let i=1;i<=r;i++)
+{
+    console.log('r=i+1')
+    
+}
+
+
+//sum of natural numbers with the help of for loop//
+
+let sum =0;
+const k = 100;
+for(let i=0;i<=k;i++)
+{
+    sum = sum +i;
+}
+
+console.log('sum :', sum);
+
+
+
